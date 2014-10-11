4070ae545d1281be2739ad553379d354311b9e6b
Ruben Cruz <ssrcruz@gmail.com>
2014-10-11 13:18:56 -0400

    created home, about, scholarships and schools pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+ root 'pages#home'
+ get "about" => "pages#about" # creates about_path
+ get "scholarships" => "pages#scholarships" # creates scholarships_path
+ get "schools" => "pages#schools" # creates schools_path
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
